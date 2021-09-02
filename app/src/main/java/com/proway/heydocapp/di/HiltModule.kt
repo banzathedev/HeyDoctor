3dd4ebef1fd0119fe4c16ea9c60b4473245eab84
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-09-02 16:38:57 -0300

    Providers for Repositories add at Hilt Modules

--- app/src/main/java/com/proway/heydocapp/di/HiltModule.kt
+++ app/src/main/java/com/proway/heydocapp/di/HiltModule.kt
@@ -2,6 +2,9 @@ package com.proway.heydocapp.di
 
 import android.content.Context
 import com.proway.heydocapp.database.AppDataBase
+import com.proway.heydocapp.repositoy.DocRepository
+import com.proway.heydocapp.repositoy.PatientsRepository
+import com.proway.heydocapp.repositoy.SpecialtiesRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,9 +15,20 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 object HiltModule {
 
-    @Provides  //injetcs database aready with the context.
+    @Provides  /* injetcs database aready with the context. */
     fun getDb(@ApplicationContext context: Context): AppDataBase{
         return AppDataBase.getDatabase(context)
     }
+
+                /* This guys privides the repositories */
+    @Provides
+    fun getSpecialtyRepository(dataBase: AppDataBase): SpecialtiesRepository = SpecialtiesRepository(dataBase)
+
+    @Provides
+    fun getDoctorRepository(dataBase: AppDataBase): DocRepository = DocRepository(dataBase)
+
+    @Provides
+    fun getPacientRepository(dataBase: AppDataBase): PatientsRepository = PatientsRepository(dataBase)
+
 }
 
