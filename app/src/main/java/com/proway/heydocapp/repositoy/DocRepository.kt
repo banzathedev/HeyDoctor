d5ae56ddd75293448363ee3089a684c2732e0fcc
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-09-02 16:25:03 -0300

    Doctors Repository Finished for now

---
+++ app/src/main/java/com/proway/heydocapp/repositoy/DocRepository.kt
@@ -0,0 +1,26 @@
+package com.proway.heydocapp.repositoy
+
+import com.proway.heydocapp.database.AppDataBase
+import com.proway.heydocapp.model.DoctorWithSpecialties
+import com.proway.heydocapp.model.DoctorsTable
+
+import javax.inject.Inject
+
+class DocRepository @Inject constructor(private val database: AppDataBase) {
+    fun getDoctors(callback: (List<DoctorWithSpecialties>?, String?) -> Unit){
+        database.getDoctorsDao().getDocWithSp().let { foundDoctors ->
+            if(foundDoctors != null){
+                callback(foundDoctors, null)
+            } else callback(null, "Nenhum Doutor Encontrado")
+
+        }
+    }
+    fun addDoctor(doctor: DoctorsTable ,callback: (Boolean) -> Unit){
+        database.getDoctorsDao().insertDoctor(doctor)
+        callback(true)
+    }
+    fun addDoctorWithSpecialties(doctor: DoctorWithSpecialties, callback: (Boolean) -> Unit){
+        database.getDoctorsDao().insertDoctor(doctor)
+        callback(true)
+    }
+}
