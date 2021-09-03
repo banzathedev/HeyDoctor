607b36c6104d23921c6b852a93877f0388dea660
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-09-03 16:48:08 -0300

    Appointments Add At Database

--- app/src/main/java/com/proway/heydocapp/database/AppDataBase.kt
+++ app/src/main/java/com/proway/heydocapp/database/AppDataBase.kt
@@ -4,22 +4,25 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.proway.heydocapp.database.DAO.AppointmentsDAO
 import com.proway.heydocapp.database.DAO.DoctorDAO
 import com.proway.heydocapp.database.DAO.PatientDao
 import com.proway.heydocapp.database.DAO.SpecialtiesDAO
+import com.proway.heydocapp.model.AppointMentsTable
 import com.proway.heydocapp.model.DoctorsTable
 
 import com.proway.heydocapp.model.PatientsTable
 import com.proway.heydocapp.model.SpecialtiesTable
 
 @Database(
-    entities = [DoctorsTable::class, PatientsTable::class, SpecialtiesTable::class],
+    entities = [DoctorsTable::class, PatientsTable::class, SpecialtiesTable::class, AppointMentsTable::class],
     version = 2
 )
 abstract class AppDataBase : RoomDatabase() {
     abstract fun getPatientDao(): PatientDao
     abstract fun getDoctorsDao(): DoctorDAO
     abstract fun getSpecialtiesDao(): SpecialtiesDAO
+    abstract fun getAppointmentsDao(): AppointmentsDAO
 
     companion object {
         fun getDatabase(context: Context): AppDataBase {
