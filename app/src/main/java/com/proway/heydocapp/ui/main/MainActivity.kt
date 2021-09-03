26667de9a5182a97d23ad533fff132e928914444
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-09-03 09:35:49 -0300

    Binding implemented

--- app/src/main/java/com/proway/heydocapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/proway/heydocapp/ui/main/MainActivity.kt
@@ -3,8 +3,10 @@ package com.proway.heydocapp.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.proway.heydocapp.R
+import com.proway.heydocapp.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,4 +17,5 @@ class MainActivity : AppCompatActivity() {
                 .commitNow()
         }
     }
+
 }
