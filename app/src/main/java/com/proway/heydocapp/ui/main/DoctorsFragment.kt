785404cb40d81b1e293399cb0e8cbebe942d5dd5
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-09-03 08:45:22 -0300

    Doctors Fragment Created

--- app/src/main/java/com/proway/heydocapp/ui/main/DoctorsFragment.kt
+++ app/src/main/java/com/proway/heydocapp/ui/main/DoctorsFragment.kt
@@ -7,26 +7,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.proway.heydocapp.R
-import com.proway.heydocapp.viewModel.PatientViewModel
+import com.proway.heydocapp.viewModel.DoctorsViewModel
 
-class PatientFragment : Fragment() {
+class DoctorsFragment : Fragment() {
 
     companion object {
-        fun newInstance() = PatientFragment()
+        fun newInstance() = DoctorsFragment()
     }
 
-    private lateinit var viewModel: PatientViewModel
+    private lateinit var viewModel: DoctorsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.patient_fragment, container, false)
+        return inflater.inflate(R.layout.doctors_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(PatientViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(DoctorsViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
