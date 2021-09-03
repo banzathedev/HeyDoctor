8df1401b16d7a2b538e4491d624b34e36b453b84
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-09-03 08:48:51 -0300

    Appointments Fragment Created

--- app/src/main/java/com/proway/heydocapp/ui/main/AppointmentsFragment.kt
+++ app/src/main/java/com/proway/heydocapp/ui/main/AppointmentsFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.proway.heydocapp.R
+import com.proway.heydocapp.viewModel.AppointmentsViewModel
 
 class AppointmentsFragment : Fragment() {
 
