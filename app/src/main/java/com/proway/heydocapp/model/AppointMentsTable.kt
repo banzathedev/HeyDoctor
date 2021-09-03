721ea8ea9a7a2ee0dc7e3ec040782acd3788d591
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-09-03 16:31:39 -0300

    Appointments Table DONE

---
+++ app/src/main/java/com/proway/heydocapp/model/AppointMentsTable.kt
@@ -0,0 +1,29 @@
+package com.proway.heydocapp.model
+
+import androidx.room.*
+
+@Entity
+data class AppointMentsTable(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "apt_id")
+    var id: Int = 0,
+    val patientFK: Int,
+    val doctorFK: Int
+)
+
+data class AppointmentsPojo(
+    @Embedded
+    val appointments: AppointMentsTable,
+
+    @Relation(
+        parentColumn = "patientFK",
+        entityColumn = "patPatient_id"
+    )
+    val patient: PatientsTable,
+
+    @Relation(
+        parentColumn = "doctorFK",
+        entityColumn = "Doctors_id"
+    )
+    val doctors: DoctorsTable
+)
