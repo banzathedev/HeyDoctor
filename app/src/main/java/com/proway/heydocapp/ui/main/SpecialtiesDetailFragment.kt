5952d989fc14226e1d9fece29b628e09a95f3117
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-09-03 13:52:11 -0300

    Specialties xmls done

---
+++ app/src/main/java/com/proway/heydocapp/ui/main/SpecialtiesDetailFragment.kt
@@ -0,0 +1,32 @@
+package com.proway.heydocapp.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.proway.heydocapp.R
+
+class SpecialtiesDetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SpecialtiesDetailFragment()
+    }
+
+    private lateinit var viewModel: SpecialtiesDetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.specialties_detail_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SpecialtiesDetailViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
